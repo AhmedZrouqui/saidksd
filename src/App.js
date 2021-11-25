cc94878e9038cfbf360949773f751937494901c9
ahmed <zrouqui.ahmed.az@gmail.com>
2021-11-25 16:36:53 +0100

    gsap

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
 import './App.scss';
 import {ReactComponent as Menu} from './menu.svg'
+import {useEffect, useState, useRef} from 'react'
+import images from './images'
+import {TweenMax, Power3} from 'gsap';
 
 function App() {
 
+  let link1 = useRef(null)
+  let dot1 = useRef(null)
+  let link2 = useRef(null)
+  let dot2 = useRef(null)
+  let link3 = useRef(null)
+  let dot3 = useRef(null)
+  let link4 = useRef(null)
+
+
   const onMenuClick = (e) => {
     e.preventDefault();
-    const menu = document.getElementById("menu");
-    menu.classList.toggle("expand-section");
+    const app = document.getElementById("main");
+    app.classList.toggle("expand-menu");
+    if(app.classList.contains("expand-menu")){
+      TweenMax.staggerFrom([link1, dot1, link2, dot2, link3, dot3, link4], .8, {opacity:0, y: 40, ease: Power3.easeOut}, .2)
+    }
+    else{
+      TweenMax.staggerTo([link4, dot3, link3, dot2, link2, dot1, link1], .8, {opacity:0, y: 40, ease: Power3.easeOut}, .2)
+    }
   }
 
   return (
-    <div className="App">
+    <div className="App" id="main">
       <div className="left-section" id="menu">
         <div className="menu-button" onClick={onMenuClick}>
           <Menu />
         </div>
         <section>
           <ul>
-            <li>Work</li>
-            <li className="dot">•</li>
-            <li>Journal</li>
-            <li className="dot">•</li>
-            <li>About</li>
-            <li className="dot">•</li>
-            <li>Contact</li>
+            <li ref={el => link1 = el}>Work</li>
+            <li ref={el => dot1 = el} className="dot">•</li>
+            <li ref={el => link2 = el}>Journal</li>
+            <li ref={el => dot2 = el} className="dot">•</li>
+            <li ref={el => link3 = el}>About</li>
+            <li ref={el => dot3 = el} className="dot">•</li>
+            <li ref={el => link4 = el}>Contact</li>
           </ul>
           <h2>
             SAID <br/>
@@ -40,7 +58,7 @@ function App() {
       </div>
       <div className="right-section">
         <article>
-          article1
+          <img src={images.image1}></img>
         </article>
         <article>
           article1
---
+++ src/images.js
@@ -0,0 +1,8 @@
+import image1 from './images/saidksd1.png'
+
+
+const images = {
+    image1
+}
+
+export default images

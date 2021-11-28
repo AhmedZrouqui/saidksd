bec6a0858cd3af07c6530da04686a6635ee2db06
zrouquiAhmed <zrouqui.ahmed.Az@gmail.com>
2021-11-28 13:54:05 +0100

    style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.scss';
 import {ReactComponent as Menu} from './menu.svg'
-import {useEffect, useState, useRef} from 'react'
+import {useRef} from 'react'
 import images from './images'
 import {TweenMax, Power3} from 'gsap';
+import CustomCursor from './components/CustomCursor';
 
 function App() {
 
@@ -13,22 +14,49 @@ function App() {
   let link3 = useRef(null)
   let dot3 = useRef(null)
   let link4 = useRef(null)
+  let first = useRef(null)
+  let second = useRef(null)
+
+  let presentation = useRef(null)
+
 
 
   const onMenuClick = (e) => {
     e.preventDefault();
     const app = document.getElementById("main");
-    app.classList.toggle("expand-menu");
-    if(app.classList.contains("expand-menu")){
-      TweenMax.staggerFrom([link1, dot1, link2, dot2, link3, dot3, link4], .8, {opacity:0, y: 40, ease: Power3.easeOut}, .2)
-    }
-    else{
-      TweenMax.staggerTo([link4, dot3, link3, dot2, link2, dot1, link1], .8, {opacity:0, y: 40, ease: Power3.easeOut}, .2)
-    }
+
+    
+
+      if(app.classList.contains("expand-menu")){
+
+        TweenMax.staggerTo([link4, dot3, link3, dot2, link2, dot1, link1], .8, {opacity:0, y: 40, ease: Power3.easeOut}, .2)
+
+        TweenMax.to(first, 2, {opacity: 0, x: -40, ease: Power3.easeOut}, 1)
+        TweenMax.to(second, 2, {opacity: 0, x: -40, ease: Power3.easeOut}, 1.2)
+        TweenMax.to(presentation, 4, {opacity: 0, ease: Power3.easeOut}, 1.5)
+
+        setTimeout(() => {
+          app.classList.remove("expand-menu");
+        }, 1500)
+      }
+      else{
+        app.classList.add("expand-menu");
+        
+        TweenMax.staggerTo([link1, dot1, link2, dot2, link3, dot3, link4], .8, {opacity:1, y: 0, ease: Power3.easeOut}, .2)
+        //TweenMax.staggerFrom([text1, text2, text3, text4, text5], .8, {opacity:0, x: 40, ease: Power3.easeOut}, .5)
+
+        TweenMax.to(first, 2, {opacity: .1, x: 0, ease: Power3.easeOut}, 2)
+        TweenMax.to(second, 2, {opacity: .1, x: 0, ease: Power3.easeOut}, 2.2)
+        TweenMax.to(presentation, 4, {opacity: 1, ease: Power3.easeOut}, 2.6)
+      
+      }
   }
 
   return (
+    <>
+    
     <div className="App" id="main">
+      <CustomCursor />
       <div className="left-section" id="menu">
         <div className="menu-button" onClick={onMenuClick}>
           <Menu />
@@ -43,16 +71,18 @@ function App() {
             <li ref={el => dot3 = el} className="dot">•</li>
             <li ref={el => link4 = el}>Contact</li>
           </ul>
-          <h2>
+          <h2 ref={el => first = el} className="name first">
             SAID <br/>
+          </h2>
+          <h2 ref={el => second = el} className="name second">
             KASSED
           </h2>
-          <h1>
-            I am a Paris based designer & art director<br/>
-            with a focus on user experience and interaction.<br/>
-            I create & build digital products for people and brands.<br/>
-            This is a feed of my work for desktops, mobile devices & installations.<br/>
-            I am available for freelance. Get in touch or Follow me.<br/>
+          <h1 ref={el => presentation = el}>
+            <span >I am a Paris based designer & art director<br/></span>
+            <span >with a focus on user experience and interaction.<br/></span>
+            <span >I create & build digital products for people and brands.<br/></span>
+            <span >This is a feed of my work for desktops, mobile devices & installations.<br/></span>
+            <span >I am available for freelance. Get in touch or Follow me.<br/></span>
           </h1>
         </section>
       </div>
@@ -61,34 +91,35 @@ function App() {
           <img src={images.image1}></img>
         </article>
         <article>
-          article1
+          <img src={images.image2}></img>
         </article>
         <article>
-          article1
+          <img src={images.image3}></img>
         </article>
         <article>
-          article1
+          <img src={images.image2}></img>
         </article>
         <article>
-          article1
+          <img src={images.image3}></img>
         </article>
         <article>
-          article1
+          <img src={images.image1}></img>
         </article>
         <article>
-          article1
+          <img src={images.image2}></img>
         </article>
         <article>
-          article1
+          <img src={images.image1}></img>
         </article>
         <article>
-          article1
+          <img src={images.image2}></img>
         </article>
         <article>
-          article1
+          <img src={images.image3}></img>
         </article>
       </div>
     </div>
+    </>
   );
 }
 
--- src/images.js
+++ src/images.js
@@ -1,8 +1,12 @@
 import image1 from './images/saidksd1.png'
+import image2 from './images/saidksd2.png'
+import image3 from './images/saidksd3.png'
 
 
 const images = {
-    image1
+    image1,
+    image2,
+    image3
 }
 
 export default images
